c8bf8fe9a79773e7e601f6b3089214671c1173b9
Jhonny <aranajhonny@users.noreply.github.com>
2022-12-08 10:35:47 -0500

    @

--- index.ts
+++ index.ts
@@ -5,31 +5,37 @@ import { nodes, root, state } from "membrane";
 
 state.tasks = [];
 
-export const Root = {
-  getAllTask: async ({ args }) => {
-    console.log(JSON.stringify(state.tasks));
-  },
-  getTask: async ({ args }) => {
-    const result = state.tasks.find((task) => task.id === args.id);
-    console.log(JSON.stringify(result));
-  },
-  addTask: async ({ args }) => {
-    state.tasks.push({
-      id: args.id,
-      title: args.title,
-      dueDate: args.dueDate,
-    });
-  },
-  editTask: async ({ args }) => {
-    const result = state.tasks.find((task) => task.id === args.id);
-    result.title = args.title;
-    result.dueDate = args.dueDate;
-  },
-  deleteTask: async ({ args }) => {
-    const index = state.tasks.findIndex((task) => task.id === args.id);
-    delete state.tasks[index];
-  },
-};
+export async function getAllTask({ args }) {
+  return state.tasks;
+}
+
+export async function getTask({ args }) {
+  return state.tasks.find((task) => task.id === args.id);
+}
+
+export async function addTask({ args }) {
+  state.tasks.push({
+    id: args.id,
+    title: args.title,
+    dueDate: args.dueDate,
+  });
+}
+
+export async function editTask({ args }) {
+  const result = state.tasks.find((task) => task.id === args.id);
+  result.title = args.title ?? result.title;
+  result.dueDate = args.dueDate ?? result.dueDate;
+}
+
+export async function deleteTask({ args }) {
+  const index = state.tasks.findIndex((task) => task.id === args.id);
+  delete state.tasks[index];
+}
+
+export async function endpoint({ args: { path, query, headers, body } }) {
+  return await nodes.html.formFor({ action: "root.addTask" }).$invoke();
+}
+
 // interface Task {
 //   id: number,
 //   title: string,
